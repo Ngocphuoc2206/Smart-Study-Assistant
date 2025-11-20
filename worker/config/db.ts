4c54fb67877e22a3efa8805091d97c99bcb48112
Ngocphuoc2206 <tranngocphuoc.2000vta@gmail.com>
2025-11-20 09:10:43 +0700

    Add example.env

--- worker/config/db.ts
+++ worker/config/db.ts
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI;
-    console.log("[DB] MONGODB_URI =", uri);
     if (!uri) {
       throw new Error("MONGODB_URI is missing in .env");
     }
 
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {dbName: process.env.MONGODB_DBNAME});
     console.log("[DB] Connected to MongoDB");
   } catch (error) {
     console.error("[DB] MongoDB connection error:", error);
